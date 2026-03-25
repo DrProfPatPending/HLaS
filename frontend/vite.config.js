efebeaa5b6177b3cadbdfabe1ad19cfebc350c55
drprofpatpending <rob@scoffin.com>
2026-03-25 14:01:25 +0000

    Added multiple entry points support to the Vite build on the frontend.

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,3 +1,4 @@
+
 import { defineConfig } from 'vite';
 import vue from '@vitejs/plugin-vue';
 import path from 'path';
@@ -7,6 +8,14 @@ export default defineConfig({
   resolve: {
     alias: {
       '@': path.resolve(__dirname, 'src'),
+    },
+  },
+  build: {
+    rollupOptions: {
+      input: {
+        main: path.resolve(__dirname, 'index.html'),
+        admin: path.resolve(__dirname, 'admin.html'),
+      },
     },
   },
   server: {
