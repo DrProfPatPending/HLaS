5a4c023597871a4db65ad955177051cc05bd799e
drprofpatpending <rob@scoffin.com>
2026-03-26 23:39:46 +0000

    Fixed the admin password script.

--- backend/update_admin_password.py
+++ backend/update_admin_password.py
@@ -1,6 +1,6 @@
 from sqlalchemy import create_engine, text
 
-engine = create_engine('postgresql+psycopg2://hlas:hlas@localhost/hlas', future=True)
+engine = create_engine('postgresql+psycopg2://hlas:hlas@localhost:5433/hlas', future=True)
 
 password_hash = 'scrypt:32768:8:1$1J7UuF8zEsZ0OTe8$54340c71e82ddaa0d110029cb9913e08aaeb176d001998e02aa4c453eb4273be81da556090129996d21e3bf31a3ddcd2da7cbade20af49610db2752fc5406e79'
 
